8983f0022f83de264d80e3ac2db9a715f9a10d77
rhgoncalves <rhgoncalves@usf.edu>
2025-04-26 14:49:48 -0700

    Update getpdf.py

--- getpdf.py
+++ getpdf.py
@@ -1,26 +1,9 @@
-from flask import Flask, request, jsonify
-import io
-from PyPDF2 import PdfReader
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/extract_pdf_form', methods=['POST'])
-def extract_pdf_form():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file uploaded'}), 400
+@app.route("/")
+def root():
+    return jsonify({"message": "Hello World"})
     
-    file = request.files['file']
-    pdf_bytes = file.read()
-
-    reader = PdfReader(io.BytesIO(pdf_bytes))
-    fields = {}
-
-    if reader.get_fields():
-        for field_name, field_info in reader.get_fields().items():
-            fields[field_name] = field_info.get('/V', '')
-
-    return jsonify(fields)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)  # Listen on port 8000 for Azure
 
