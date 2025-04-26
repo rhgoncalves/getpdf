cca58b454b8df52c86c1190ed2cd01f8f862cbd4
rhgoncalves <rhgoncalves@usf.edu>
2025-04-26 11:26:45 -0700

    Update getpdf.py

--- getpdf.py
+++ getpdf.py
@@ -5,7 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     return jsonify({"message": "Hello World test 1"})
-
-# Correct way to run a Flask app in development
-if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=8000)
