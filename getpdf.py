215d99f53f0b5fcd5f037cbd25d97982c692f154
rhgoncalves <rhgoncalves@usf.edu>
2025-04-26 11:01:41 -0700

    Update getpdf.py

--- getpdf.py
+++ getpdf.py
@@ -7,4 +7,9 @@ def root():
     return jsonify({"message": "Hello World"})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)
+        app = init_func(None)
+
+try:
+        web.run_app(app, host="0.0.0.0", port=CONFIG.PORT)
+    except Exception as error:
+        raise error
