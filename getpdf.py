804ec90e4e3dedefb6500f0af961b97965610330
rhgoncalves <rhgoncalves@usf.edu>
2025-04-26 06:02:22 -0700

    Update getpdf.py

--- getpdf.py
+++ getpdf.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def root():
     return jsonify({"message": "Hello World"})
-if __name__=="__main__"
-    app.run(debug=True,host = "0.0.0.0",port=8000)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8000)
