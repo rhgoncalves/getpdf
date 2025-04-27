925d1bbca90c0975d0fdda02d6f61c65a39b8e4b
rhgoncalves <rhgoncalves@usf.edu>
2025-04-26 18:03:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 def root():
     return jsonify({"message": "Hello World"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)  # Ensure Flask listens on port 8080
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
 
